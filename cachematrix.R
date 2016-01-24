e1393a6b169a803a2593baf40afacfa4c645b7eb
allerdien <allerdien@gmail.com>
2016-01-24 14:47:01 +0100

    Create cachematrix.R

---
+++ cachematrix.R
@@ -0,0 +1,37 @@
+## Put comments here that give an overall description of what your
+## functions do
+
+## Write a short comment describing this function
+
+makeCacheMatrix <- function(x = matrix()) {
+  m<-NULL
+# M(matrix) is undefined
+  set<-function(y){
+  x<<-y
+# gives you the inverse of x in y
+  m<<-NULL
+}
+get<-function() x
+setmatrix<-function(solve) m<<- solve
+# assigns the solve not to be undefined(so defined) 
+getmatrix<-function() m
+list(set=set, get=get,
+   setmatrix=setmatrix,
+   getmatrix=getmatrix)
+}
+
+## Write a short comment describing this function
+cacheSolve <- function(x=matrix(), ...) {
+    m<-x$getmatrix()
+    if(!is.null(m)){
+      message("getting cached data")
+      return(m)
+    }
+    matrix<-x$get()
+    m<-solve(matrix, ...)
+    x$setmatrix(m)
+    m
+}
+
+ ## Return a matrix that is the inverse of 'x'
+
